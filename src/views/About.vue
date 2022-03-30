d299127327fd238be9e5c20a4779ae96e3fd6e95
jiaxiaolei <jiaxiaolei@mbp.cn.ibm.com>
2022-03-30 15:35:23 +0800

    dynamically add new page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="toCustomPage">Add Custome Page</button>
   </div>
 </template>
+<script>
+export default {
+  methods: {
+    toCustomPage() {
+      console.log('add route')
+      this.$router.addRoutes([{path: '/custom', component: () => import('@/views/Custom.vue'), name: 'custom'}])
+    }
+  }
+}
+</script>
